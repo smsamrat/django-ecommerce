b7a73aedcc00b69adb56b52c3a16a8fa35ff6c4c
you <97931972+smsamrat@users.noreply.github.com>
2022-02-02 12:34:14 +0600

    some changes for checkout page

--- payment/views.py
+++ payment/views.py
@@ -17,7 +17,7 @@ class CheckoutTemplateView(TemplateView):
         order_qs = order.objects.filter(user = request.user, ordered=False)
         order_item = order_qs[0].orders_item.all()
         order_totals = order_qs[0].order_item_total()
-        total_price_after_discount = order_qs[0].total_price_after_discount()
+
 
 
 
@@ -25,7 +25,6 @@ class CheckoutTemplateView(TemplateView):
             'billing_address':form,
             'order_item':order_item,
             'order_totals':order_totals,
-            'total_price_after_discount':total_price_after_discount,
         }
         return render(request, 'store/checkout.html',context)
 
