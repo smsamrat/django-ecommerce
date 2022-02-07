92f34eb089e5025efdbe742e75598c96e1a46ad1
you <97931972+smsamrat@users.noreply.github.com>
2022-02-07 18:21:54 +0600

    Dynamic search product

--- store/views.py
+++ store/views.py
@@ -1,21 +1,32 @@
+from msilib.schema import Class
+from re import template
 from django.http import request
 from django.shortcuts import render
 from django.template import context
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView, TemplateView
 from store.models import Product, ProductImages, BannerImage
 
+class ProductListView(TemplateView):
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'store/index.html'
-    context_object_name = 'products'
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all().order_by('-id')
+        bannerimage = BannerImage.objects.filter(is_active=True).order_by('-id')
+        context = {
+            'products':products,
+            'bannerimage':bannerimage,
+        }
+        return render(request, 'store/index.html',context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['bannerimage']= BannerImage.objects.filter(is_active=True).order_by('-id')
-        return context
-    
+    def post(self, request, *args, **kwargs):
+            if request.method == 'post' or request.method == 'POST':
+                product_search = request.POST.get('product_search')
+                products = Product.objects.filter(name__icontains=product_search).order_by('-id')
+                context = {
+                    'products':products
+                }
+                return render(request, 'store/index.html',context)
 
+                
 class Product_Details(DetailView):
     model = Product
     template_name= 'store/product.html'
@@ -25,7 +36,6 @@ class Product_Details(DetailView):
         context = super().get_context_data(**kwargs)
         context['product_images']=ProductImages.objects.filter(product=self.object.id)
         return context
-    
 
     
     # def get_context_data(self, **kwargs):
